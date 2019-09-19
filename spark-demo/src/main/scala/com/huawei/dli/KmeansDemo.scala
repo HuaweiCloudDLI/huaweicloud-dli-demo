434c41ba678b89651cebedfcbde87bc292fc8782
wangzhenhua <wangzhenhua@huawei.com>
2019-09-19 15:24:48 +0800

    use a better way to set ak/sk

--- spark-demo/src/main/scala/com/huawei/dli/KmeansDemo.scala
+++ spark-demo/src/main/scala/com/huawei/dli/KmeansDemo.scala
@@ -1,32 +1,36 @@
-package com.huawei.dli
+package com.huawei.demo
 
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.mllib.clustering.KMeans
 import org.apache.spark.mllib.linalg.Vectors
 
-object KmeansDemo {
+object KMeansDemo {
+
   def main(args: Array[String]): Unit = {
 
-    if (args.length != 2) {
-      println(s"Wrongnumber of arguments! Expected 2 but got ${args.length}")
+    if (args.length != 4) {
+      println(s"Wrong number of arguments! Expected 4 but got ${args.length}")
       return
     }
 
-    // your AK/SK to access OBS
-    val AK = "******"
-    val SK = "******"
+    // Your AK/SK to access OBS
+    val ak = args(0)
+    val sk = args(1)
 
-    val prefix = "s3a://" + AK + ":" + SK + "@"
-    val readPath = prefix + args(0)
-    val writePath = prefix + args(1)
+    val readPath = args(2)
+    val writePath = args(3)
 
     val conf = new SparkConf().setAppName("KMeansExample")
     val sc = new SparkContext(conf)
+    sc.hadoopConfiguration.set("fs.s3a.access.key", ak)
+    sc.hadoopConfiguration.set("fs.s3a.secret.key", sk)
 
     // Load and parse the data
     val data = sc.textFile(readPath)
-    val parsedData = data.map { s =>
+    val header = data.first()
+    val parsedData = data.filter(_ != header).map { s =>
       val attributes = s.split(",")
+      // Use attributes 'latitude' and 'longitude' as the clustering features
       val array = Seq(attributes(16).toDouble, attributes(17).toDouble).toArray
       Vectors.dense(array)
     }
@@ -35,12 +39,15 @@ object KmeansDemo {
     val numClusters = 2
     val numIterations = 20
     val clusters = KMeans.train(parsedData, numClusters, numIterations)
+
     // Evaluate clustering by computing Within Set Sum of Squared Errors
     val WSSSE = clusters.computeCost(parsedData)
     println("Within Set Sum of Squared Errors = " + WSSSE)
 
-    // Save and load model
+    // Save the model
     clusters.save(sc, writePath)
+
     sc.stop()
   }
 }
+
